714af23419470b0c1f500d4e5b1b3ca56e64045b
hendra_foo <fu.ceyong18@gmail.com>
2020-08-25 22:24:34 +0700

    Updates

---
+++ precache-manifest.a66309552750ef9ad80518a335f78c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea904760e2e900759be3e890d7eb43f",
+    "url": "/hendra-foo.github.io/index.html"
+  },
+  {
+    "revision": "5a84bd300379d92d497b",
+    "url": "/hendra-foo.github.io/static/css/main.bed50fa2.chunk.css"
+  },
+  {
+    "revision": "c46547c047ea757e6752",
+    "url": "/hendra-foo.github.io/static/js/2.1199ce04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hendra-foo.github.io/static/js/2.1199ce04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a84bd300379d92d497b",
+    "url": "/hendra-foo.github.io/static/js/main.a7b0cb83.chunk.js"
+  },
+  {
+    "revision": "972894c5742b29eac3ac",
+    "url": "/hendra-foo.github.io/static/js/runtime-main.f67003d2.js"
+  }
+]);
